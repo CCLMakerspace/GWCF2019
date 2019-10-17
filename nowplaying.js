a486cd788bb35aa31427eb3bcf4a5c8d5405f052
CCL-Makerspace <56364523+CCL-Makerspace@users.noreply.github.com>
2019-10-17 10:10:23 -0400

    Update nowplaying.js

--- nowplaying.js
+++ nowplaying.js
@@ -3,9 +3,9 @@ var image = document.getElementById('image');
 var random_number = Math.floor((Math.random()*total_images));
 var random_img = [];
 
-random_img[0] = '<a href="https://google.com" target="_blank"><img src="preethi.jpg"></a>';
-random_img[1] = '<a href="https://twitter.com" target="_blank"><img src="shanthi.jpg"></a>';
-random_img[2] = '<a href="https://tumblr.com" target="_blank"><img src="abhinav.jpg"></a>';
+random_img[0] = '<a href="Preethi.html" target="_blank"><img src="preethi.jpg"></a>';
+random_img[1] = '<a href="Shanthi.html" target="_blank"><img src="shanthi.jpg"></a>';
+random_img[2] = '<a href="hi.html" target="_blank"><img src="abhinav.jpg"></a>';
 random_img[3] = '<a href="https://facebook.com" target="_blank"><img src="harisha.jpg"></a>';
 random_img[4] = '<a href="https://spotify.com" target="_blank"><img src="rishika.jpg"></a>';
 random_img[5] = '<a href="https://bing.com" target="_blank"><img src="arya.jpg"></a>';
