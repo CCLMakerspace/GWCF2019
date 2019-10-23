2f3502ddeb4065322ef80ffcf76a346ee0115182
CCL-Makerspace <56364523+CCL-Makerspace@users.noreply.github.com>
2019-10-23 18:59:01 -0400

    Update nowplaying.js

--- nowplaying.js
+++ nowplaying.js
@@ -7,7 +7,7 @@ random_img[0] = '<a href="Preethi.html" target="_blank"><img src="preethi.jpg"><
 random_img[1] = '<a href="Shanthi.html" target="_blank"><img src="shanthi.jpg"></a>';
 random_img[2] = '<a href="hi.html" target="_blank"><img src="abhinav.jpg"></a>';
 random_img[3] = '<a href="https://facebook.com" target="_blank"><img src="harisha.jpg"></a>';
-random_img[4] = '<a href="https://spotify.com" target="_blank"><img src="rishika.jpg"></a>';
+random_img[4] = '<a href="https://www.khanacademy.org/hour-of-code/diy-halloween-costumes/5887408675094528" target="_blank"><img src="rishika.jpg"></a>';
 random_img[5] = '<a href="https://bing.com" target="_blank"><img src="arya.jpg"></a>';
 random_img[6] = '<a href="https://apple.com" target="_blank"><img src="brody.jpg"></a>';
 random_img[7] = '<a href="https://youtube.com" target="_blank"><img src="shivika.jpg"></a>';
