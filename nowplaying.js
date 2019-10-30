8a768afde8abb75eb5332ddb06322d3283fdc90d
CCL-Makerspace <56364523+CCL-Makerspace@users.noreply.github.com>
2019-10-30 19:57:57 -0400

    Update nowplaying.js

--- nowplaying.js
+++ nowplaying.js
@@ -3,12 +3,12 @@ var image = document.getElementById('image');
 var random_number = Math.floor((Math.random()*total_images));
 var random_img = [];
 
-random_img[0] = '<a href="Preethi.html" target="_blank"><img src="preethi.jpg" width="300" height="300"></a>';
-random_img[1] = '<a href="Shanthi.html" target="_blank"><img src="shanthi.jpg" width="300" height="300"></a>';
-random_img[2] = '<a href="hi.html" target="_blank"><img src="abhinav.jpg" width="300" height="300"></a>';
-random_img[3] = '<a href="https://www.khanacademy.org/hour-of-code/diy-halloween-costumes/5887408675094528" target="_blank"><img src="rishika.jpg" width="300" height="300"></a>';
-random_img[4] = '<a href="arya.html" target="_blank"><img src="arya.jpg" width="300" height="300"></a>';
-random_img[5] = '<a href="Brody-web.html" target="_blank"><img src="brody.jpg" width="300" height="300"></a>';
-random_img[6] = '<a href="mainpage.html" target="_blank"><img src="shivika.jpg" width="300" height="300"></a>';
+random_img[0] = '<a href="Preethi.html" target="_blank"><img src="preethi.jpg" width="200" height="200"></a>';
+random_img[1] = '<a href="Shanthi.html" target="_blank"><img src="shanthi.jpg" width="200" height="200"></a>';
+random_img[2] = '<a href="hi.html" target="_blank"><img src="abhinav.jpg" width="200" height="200"></a>';
+random_img[3] = '<a href="https://www.khanacademy.org/hour-of-code/diy-halloween-costumes/5887408675094528" target="_blank"><img src="rishika.jpg" width="200" height="200"></a>';
+random_img[4] = '<a href="arya.html" target="_blank"><img src="arya.jpg" width="200" height="200"></a>';
+random_img[5] = '<a href="Brody-web.html" target="_blank"><img src="brody.jpg" width="200" height="200"></a>';
+random_img[6] = '<a href="mainpage.html" target="_blank"><img src="shivika.jpg" width="200" height="200"></a>';
 
 image.innerHTML = random_img[random_number];
